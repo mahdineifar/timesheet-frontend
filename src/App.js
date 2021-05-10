face8b0738bc1367766199acf07c28a60b45b87f
mneifar <mahdi.neifar123@gmail.com>
2021-05-10 15:15:35 +0100

    add linter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './scss/style.scss';
 import {
   CButton,
@@ -19,48 +19,48 @@ import { freeSet } from '@coreui/icons';
 
 function App() {
   return (
-      <div className="c-app c-default-layout flex-row align-items-center">
-        <CContainer>
-          <CRow className="justify-content-center">
-            <CCol md="5">
-              <CCardGroup>
-                <CCard className="p-4">
-                  <CCardBody>
-                    <CForm>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
-                      <CInputGroup className="mb-3">
-                        <CInputGroupPrepend>
-                          <CInputGroupText>
-                            <CIcon content={freeSet.cilUser} />
-                          </CInputGroupText>
-                        </CInputGroupPrepend>
-                        <CInput type="text" placeholder="Username" autoComplete="username" />
-                      </CInputGroup>
-                      <CInputGroup className="mb-4">
-                        <CInputGroupPrepend>
-                          <CInputGroupText>
-                            <CIcon content={freeSet.cilLockLocked} />
-                          </CInputGroupText>
-                        </CInputGroupPrepend>
-                        <CInput type="password" placeholder="Password" autoComplete="current-password" />
-                      </CInputGroup>
-                      <CRow>
-                        <CCol xs="6">
-                          <CButton color="primary" className="px-4">Login</CButton>
-                        </CCol>
-                        <CCol xs="6" className="text-right">
-                          <CButton color="link" className="px-0">Forgot password?</CButton>
-                        </CCol>
-                      </CRow>
-                    </CForm>
-                  </CCardBody>
-                </CCard>
-              </CCardGroup>
-            </CCol>
-          </CRow>
-        </CContainer>
-      </div>
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="5">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon content={freeSet.cilUser} />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="text" placeholder="Username" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon content={freeSet.cilLockLocked} />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="password" placeholder="Password" autoComplete="current-password" />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="6">
+                        <CButton color="primary" className="px-4">Login</CButton>
+                      </CCol>
+                      <CCol xs="6" className="text-right">
+                        <CButton color="link" className="px-0">Forgot password?</CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
   );
 }
 
