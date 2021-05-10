ba2609b2cfeb50f50ba4f7256474d7e72860b388
mneifar <mahdi.neifar123@gmail.com>
2021-05-10 15:25:00 +0100

    add routing to App component

---
+++ src/views/Login.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm, CInput,
+  CInputGroup,
+  CInputGroupPrepend, CInputGroupText,
+  CRow,
+} from '@coreui/react';
+import CIcon from '@coreui/icons-react';
+import { freeSet } from '@coreui/icons';
+
+const Login = () => (
+  <div className="c-app c-default-layout flex-row align-items-center">
+    <CContainer>
+      <CRow className="justify-content-center">
+        <CCol md="5">
+          <CCardGroup>
+            <CCard className="p-4">
+              <CCardBody>
+                <CForm>
+                  <h1>Login</h1>
+                  <p className="text-muted">Sign In to your account</p>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupPrepend>
+                      <CInputGroupText>
+                        <CIcon content={freeSet.cilUser} />
+                      </CInputGroupText>
+                    </CInputGroupPrepend>
+                    <CInput type="text" placeholder="Username" autoComplete="username" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-4">
+                    <CInputGroupPrepend>
+                      <CInputGroupText>
+                        <CIcon content={freeSet.cilLockLocked} />
+                      </CInputGroupText>
+                    </CInputGroupPrepend>
+                    <CInput type="password" placeholder="Password" autoComplete="current-password" />
+                  </CInputGroup>
+                  <CRow>
+                    <CCol xs="6">
+                      <CButton color="primary" className="px-4">Login</CButton>
+                    </CCol>
+                    <CCol xs="6" className="text-right">
+                      <CButton color="link" className="px-0">Forgot password?</CButton>
+                    </CCol>
+                  </CRow>
+                </CForm>
+              </CCardBody>
+            </CCard>
+          </CCardGroup>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </div>
+);
+
+export default Login;
